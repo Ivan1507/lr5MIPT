b5d4dea238ad1576b106559afdd2fb6bb5f2b138
Ivan <iancurtis1507@mail.ru>
2020-05-15 00:01:01 +0300

    workhard

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
-
 using namespace std;
-
+void print(int A[],int n){
+for(int i=0;i<n;i++){
+    cout<<A[i]<<" ";
+}
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int n;
+    cin>>n;
+    int A[n];
+    bool F[n];
+    for(int i=0;i<n;i++){
+        cin>>A[i];
+        if(A[i]>0) F[i]=true;
+        else F[i]=false;
+    }
+    for(int i=0;i<n-1;i++){
+       for(int j=0;j<n-i-1;j++){
+        if(A[j]<0&&A[j+1]<0){
+                if(A[j]<A[j+1]){
+                    int t=A[j];
+            A[j]=A[j+1];
+            A[j+1]=t;
+            print(A,n);
+            cout<<endl;
+                }
+            }
+            else if(A[j]>0&&A[j+1]>0){
+                if(A[j]>A[j+1]){
+                    int t=A[j];
+            A[j]=A[j+1];
+            A[j+1]=t;
+            print(A,n);
+            cout<<endl;
+       }
+    }}
+    }
+
+
+
+
 }
